f047fe6f2c631a0f7dea7e42840dd91a2ef208e9
evergreenacorn <rasakjustone01.11@gmail.com>
2021-06-28 15:11:19 +0300

    Зарегестрированы в админке все основные модели приложения app, с историей их изменений.

--- app/pages/admin.py
+++ app/pages/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from . import models
 
 
@@ -9,4 +10,4 @@ for model in (
     models.ContentTypeAudio,
     models.ContentTypeText
 ):
-    admin.site.register(model)
+    admin.site.register(model, SimpleHistoryAdmin)
