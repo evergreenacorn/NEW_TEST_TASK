db6a9ae8eeb5daa61c1ef48f9a95100253756c9b
evergreenacorn <rasakjustone01.11@gmail.com>
2021-06-28 14:52:07 +0300

    Добавлена и включена в проект библиотека для аудита django-simple-history.

--- app/test_task/settings.py
+++ app/test_task/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'simple_history',
 
     'pages',
 ]
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'test_task.urls'
