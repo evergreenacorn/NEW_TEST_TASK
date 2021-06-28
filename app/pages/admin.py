ef06d6e9f6a7f6980acd695d66c8fce88910fc62
evergreenacorn <rasakjustone01.11@gmail.com>
2021-06-28 16:10:30 +0300

    Небольшая кастомизация админки.

--- app/pages/admin.py
+++ app/pages/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 from simple_history.admin import SimpleHistoryAdmin
-from . import models
+from .models import (
+    _, Page, ContentTypeVideo,
+    ContentTypeAudio, ContentTypeText
+)
 
 
 # Register your models here.
+admin.site.site_header = _("TEST TASK Admin")
+admin.site.site_title = _("TEST TASK Admin Portal")
+admin.site.index_title = _("Welcome to the admin panel of the TEST TASK project!")
+
+
 for model in (
-    models.Page,
-    models.ContentTypeVideo,
-    models.ContentTypeAudio,
-    models.ContentTypeText
+    Page,
+    ContentTypeVideo,
+    ContentTypeAudio,
+    ContentTypeText
 ):
     admin.site.register(model, SimpleHistoryAdmin)
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
