f85b32dfdd45477e4de77137b2a666123a8ce6e4
evergreenacorn <rasakjustone01.11@gmail.com>
2021-06-29 20:30:53 +0300

    Переписал вьюхи, пока с RY, но уже с поиском и фильтрами.

--- app/pages/views.py
+++ app/pages/views.py
@@ -1,36 +1,66 @@
-from rest_framework import renderers, viewsets, generics
+from rest_framework import renderers, viewsets, generics, mixins
 from rest_framework import permissions
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.request import Request
 from .serializers import PageSerializer, ContentTypeVideoSerializer, ContentTypeAudioSerializer, ContentTypeTextSerializer
 from .models import Page, ContentTypeVideo, ContentTypeAudio, ContentTypeText
+# from rest_framework.decorators import detail_route
+from .task import update_views_count
 
 
-class PageModelViewset(viewsets.ModelViewSet):
-    # model = Page
+class PageModelViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = PageSerializer
-    queryset = Page.objects.all()
-    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Page.objects.prefetch_related("page_videos", "page_audios", "page_texts").all()
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = (
+        "title",
+        "page_videos__title",
+        "page_audios__title",
+        "page_texts__title",
+        "=page_texts__text",  # матчим текст в контенте текста
+    )
+    ordering_fields = (
+        "pk", "title",
+    )
     paginate_by = 20
 
 
-class VideoModelViewset(viewsets.ModelViewSet):
-    # model = Page
+class VideoModelViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = ContentTypeVideoSerializer
     queryset = ContentTypeVideo.objects.all()
-    # permission_classes = [permissions.IsAuthenticated]
     paginate_by = 20
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = (
+        "title", "page__title"
+    )
+    ordering_fields = (
+        "pk", "title", "serial_number", "page__pk"
+    )
 
 
-class AudioModelViewset(viewsets.ModelViewSet):
-    # model = Page
+class AudioModelViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = ContentTypeAudioSerializer
     queryset = ContentTypeAudio.objects.all()
-    # permission_classes = [permissions.IsAuthenticated]
     paginate_by = 20
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = (
+        "title", "page__title"
+    )
+    ordering_fields = (
+        "pk", "title", "serial_number", "page__pk"
+    )
 
 
-class TextModelViewset(viewsets.ModelViewSet):
-    # model = Page
+class TextModelViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = ContentTypeTextSerializer
     queryset = ContentTypeText.objects.all()
-    # permission_classes = [permissions.IsAuthenticated]
     paginate_by = 20
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = (
+        "title", "page__title", "=text"
+    )
+    ordering_fields = (
+        "pk", "title", "serial_number", "page__pk"
+    )
