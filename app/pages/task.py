4b286dd52490631b97ef07339713fe21a3f802cd
evergreenacorn <rasakjustone01.11@gmail.com>
2021-06-29 18:19:54 +0300

    Добавлена тестовая задача для увеличения счетчика просмотров контента страницы.

---
+++ app/pages/task.py
@@ -0,0 +1,14 @@
+import logging
+from .models import Page
+from django.shortcuts import get_object_or_404
+from django.db.models import F
+from ..test_task.celery import app
+
+
+@app.task
+def update_views_count(page_id):
+    page = get_object_or_404(Page, pk=page_id)
+    page_content = (page.page_videos, page.page_audios, page.page_texts)
+    for content in page_content:
+        if content is not None:
+            content.update(F("view_counter") + 1)
