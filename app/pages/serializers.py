17f93041cb4a8b815deb805e6afbf2e04185bea9
evergreenacorn <rasakjustone01.11@gmail.com>
2021-06-29 17:13:42 +0300

    Именены тестовые сериализаторы, сограсно DRY.

--- app/pages/serializers.py
+++ app/pages/serializers.py
@@ -2,25 +2,31 @@ from rest_framework import serializers
 from .models import Page, ContentTypeVideo, ContentTypeAudio, ContentTypeText
 
 
+class AbstractContentTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        fields = "__all__"
+
+
+class ContentTypeVideoSerializer(AbstractContentTypeSerializer):
+    class Meta(AbstractContentTypeSerializer.Meta):
+        model = ContentTypeVideo
+
+
+class ContentTypeAudioSerializer(AbstractContentTypeSerializer):
+    class Meta(AbstractContentTypeSerializer.Meta):
+        model = ContentTypeAudio
+
+
+class ContentTypeTextSerializer(AbstractContentTypeSerializer):
+    class Meta(AbstractContentTypeSerializer.Meta):
+        model = ContentTypeText
+
+
 class PageSerializer(serializers.HyperlinkedModelSerializer):
+    videos = ContentTypeVideoSerializer(source="page_videos", many=True)
+    audios = ContentTypeAudioSerializer(source="page_audios", many=True)
+    texts = ContentTypeTextSerializer(source="page_texts", many=True)
+
     class Meta:
         model = Page
-        fields = "__all__"
-
-
-class ContentTypeVideoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ContentTypeVideo
-        fields = "__all__"
-
-
-class ContentTypeAudioSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ContentTypeAudio
-        fields = "__all__"
-
-
-class ContentTypeTextSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ContentTypeText
         fields = "__all__"
