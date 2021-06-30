032365fd64cd58a0a9b0ef7497c64b102a0a5a67
evergreenacorn <rasakjustone01.11@gmail.com>
2021-06-30 18:04:36 +0300

    Поправлены настройки celery.

--- app/test_task/celery.py
+++ app/test_task/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_task.settings")
 
 app = Celery("test_task")
 app.config_from_object("django.conf:settings")
