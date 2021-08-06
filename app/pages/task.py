d72d1d9964e61a24c2c3b80836272dde5297fc60
eg_acorn <78857940+evergreenacorn@users.noreply.github.com>
2021-08-06 12:46:55 +0300

    Исправлен handling ошибок celery таски

--- app/pages/task.py
+++ app/pages/task.py
@@ -6,8 +6,11 @@ from test_task.celery import app
 
 @app.task
 def update_views_count(page_id):
-    page = get_object_or_404(Page, pk=page_id)
-    page_content = (page.page_videos, page.page_audios, page.page_texts)
-    for content in page_content:
-        if content is not None:
-            content.all().update(view_counter=F("view_counter") + 1)
+    try:
+        page = get_object_or_404(Page, page_id)
+        page_content = (page.page_videos, page.page_audios, page.page_texts)
+        for content in page_content:
+            if content is not None:
+                content.all().update(view_counter=F("view_counter") + 1)
+    except Exception as e:
+        raise e
